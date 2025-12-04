aceaf37235e398542c60fdd24a3fc2cc7ca050c9
C0RUJA0 <123303716+C0RUJA0@users.noreply.github.com>
2025-12-04 14:40:47 -0300

    Modify ffmpeg command for RTSP input
    Updated ffmpeg command to use TCP transport and added scaling and frame rate options.

--- uvc.py
+++ uvc.py
@@ -6,7 +6,7 @@ import time
 directory = '/opt/uvc-gadget-webcam/'
 driver_script = os.path.join(directory, 'rpi-uvc-gadget.sh')
 uvc_gadget = os.path.join(directory, 'uvc-gadget -v /dev/video1 -u /dev/video0 -n3 -f0 -r0 -s1 -o1')
-ffmpeg = 'ffmpeg -f rtsp -i rtsp://192.168.0.180:8554/webcam -pix_fmt yuyv422 -f v4l2 /dev/video1'
+ffmpeg = 'ffmpeg -rtsp_transport tcp -fflags nobuffer -i rtsp://YOUR_IP:554/YOUR_PATH -an -vf scale=640:360,fps=30 -pix_fmt yuyv422 -f v4l2 /dev/video1'
 
 result = subprocess.run([driver_script, 'start'], capture_output=True, text=True)
 print(result.stdout)
@@ -16,7 +16,6 @@ while True:
     ffmpeg_process = subprocess.Popen(ffmpeg.split(' '), start_new_session=True, stdout=subprocess.PIPE)
     time.sleep(12)
     uvc_process = subprocess.run(uvc_gadget.split(' '), capture_output=True, text=True)
-    uvc_process.wait()
     print(result.stdout)
     # If UVC gadget program completes, kill ffmpeg and loop
     ffmpeg_process.kill()
